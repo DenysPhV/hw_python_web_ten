b8ca9cf8669ab234a168f8dba2f338fc9f1e6fe5
Denys Filichkin <denys.philichkin@gmail.com>
2023-07-26 09:23:49 +0300

    changed registr on signup

--- hw_app/users/urls.py
+++ hw_app/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.signup_user, name='register'),
+    path('signup/', views.signup_user, name='signup'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 ]
